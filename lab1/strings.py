bd0359dfe634ba6b89f3438aa0adf7165a6bdf62
tansholpanbakhtiyarbek <btansholpan2702@gmail.com>
2026-02-04 01:28:38 +0500

    lab1

---
+++ lab1/strings.py
@@ -0,0 +1,26 @@
+#STRINGS 
+
+print("Hello")
+print('Hello')
+#they are same
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a="Hi"
+print(a)
+
+#three double quotes
+a = """HIURFH;EURHF;RT
+dnjcjewbcebrfhr
+kfcnjefrvhuethvguth"""
+print(a)
+
+#Strings are Arrays
+a="Hello"
+print(a[0])
+
+#String Length
+a="Hello"
+print(len(a))
