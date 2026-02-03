0fd45a52c6014af683c6159441c521517aa9d62a
tansholpanbakhtiyarbek <btansholpan2702@gmail.com>
2026-02-03 21:09:41 +0500

    task333

---
+++ lab1/syntax.py
@@ -0,0 +1,14 @@
+#first example 
+if 5>2:
+    print("five is is greater than two")
+
+#second example
+if 5<7:
+        print("five less than seven")
+
+
+#variables
+x=5
+y="Hello, World"
+
+#this is a comment 
