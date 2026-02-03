e59c79a0bd8b2b59300ba8febaf8caee91ff5a89
tansholpanbakhtiyarbek <btansholpan2702@gmail.com>
2026-02-03 21:20:13 +0500

    task4

---
+++ lab1/print_numbers.py
@@ -0,0 +1,14 @@
+#numbers 
+#first example
+print (7)
+print (67)
+print (123)
+
+#second example
+print(7+7)
+print(2*2)
+print(25/5)
+print(258//3)
+
+#mix text and numbers 
+print ("I am", 25, "years old")
