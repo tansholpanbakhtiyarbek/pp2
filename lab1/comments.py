aaff83969315d82d9bcd3f3b8873830c579d1d26
tansholpanbakhtiyarbek <btansholpan2702@gmail.com>
2026-02-03 21:29:59 +0500

    task5

---
+++ lab1/comments.py
@@ -0,0 +1,16 @@
+#this is a comment
+print("Hello")
+
+print("World!") #this is a comment 
+
+#this is a comment 
+#hello, world!
+#ikcmeofncvoejrnfv
+print ("Hello,World!")
+
+"""
+this i a comment
+written in 
+more that one line 
+"""
+print ("Hello,world")
