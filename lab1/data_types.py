deb482f931c910ac258119203cb0ce5a3b475111
tansholpanbakhtiyarbek <btansholpan2702@gmail.com>
2026-02-04 01:14:32 +0500

    task7

---
+++ lab1/data_types.py
@@ -0,0 +1,40 @@
+#1st example
+x =6
+print(type(x))
+
+
+#2nd example
+x = "Hello, World"
+print(type(x))
+
+#3rd example
+x = 21.7		
+print(type(x)) 
+
+#4th example 
+x = True		
+print(type(x))
+
+#5th example
+x = 7j		
+print(type(x))
+
+#6th example
+x = ["apple", "banana", "cherry"]		
+print(type(x))
+
+#7th example
+x = ("apple", "banana", "cherry")		
+print(type(x))
+
+#8th example
+x = None
+print(type(x))
+
+#9th example
+x = {"apple", "banana", "cherry"}		
+print(type(x))
+
+#10th example
+x = range(6)		
+print(type(x))
