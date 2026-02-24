8a3a14b91328eaa33c21c2b06d4fc2da52f0826a
tansholpanbakhtiyarbek <btansholpan2702@gmail.com>
2026-02-25 04:06:04 +0500

    Add lab4 practice04

---
+++ lab2/dictionary.py
@@ -0,0 +1,40 @@
+#dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#Dictionary Items
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#Dictionary Length
+print(len(thisdict))
+
+#type()
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#Access Dictionary Items
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+
+#get()
+x = thisdict.get("model")
+
+#Get Values
+x = thisdict.values()
